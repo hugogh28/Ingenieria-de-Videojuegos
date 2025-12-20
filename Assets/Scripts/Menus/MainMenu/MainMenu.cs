c20e29b7a1e3a682082bf66c827e83ef09164152
hiper942 <ismaelgg1612@gmail.com>
2025-12-20 06:10:59 +0100

    Voy a hacer el menuç

---
+++ Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Play()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    // Update is called once per frame
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
