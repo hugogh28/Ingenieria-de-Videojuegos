8e989bfa0012b502fc78a66e409ce492ef3ff299
Hugo <hugogarciahernandez281@gmail.com>
2026-01-03 06:20:39 +0100

    Adición de breves comentarios al código

--- Assets/Scripts/Enemy/Waves/Zone.cs
+++ Assets/Scripts/Enemy/Waves/Zone.cs
@@ -11,7 +11,7 @@ public class Zone : MonoBehaviour
     public void OnTriggerEnter(Collider other) //Detectamos si el jugador está dentro de alguna de las zonas de spawn
     {
         if (!other.CompareTag("Player")) return;
-        foreach (Transform t in transform) 
+        foreach (Transform t in transform) //Se activa cada hijo del objeto y si este no estaba en la lista de spawners, es incluido en la misma
         { 
             t.gameObject.SetActive(true);
             if(wave.spawners != null && !wave.spawners.Contains(t.gameObject) ) wave.spawners.Add(t.gameObject);
